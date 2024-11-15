0b173d828a8a5449db8f7ebaf15af5de86f66e8a
mohiwalla <hindustanjindabad5911@gmail.com>
2024-11-15 12:27:16 +0530

    added new options list

---
+++ app/config/options-list.ts
@@ -0,0 +1,711 @@
+const options = [
+	{
+		stream: "A",
+		option: "Operate a printing press",
+	},
+	{
+		stream: "B",
+		option: "Study the causes of earthquakes",
+	},
+	{
+		stream: "C",
+		option: "Plant and harvest crops",
+	},
+	{
+		stream: "R",
+		option: "Replace a car window and fender",
+	},
+	{
+		stream: "E",
+		option: "Analyze reports and records",
+	},
+	{
+		stream: "F",
+		option: "Operate a machine",
+	},
+	{
+		stream: "G",
+		option: "Work in an office",
+	},
+	{
+		stream: "H",
+		option: "Answer customer questions",
+	},
+	{
+		stream: "D",
+		option: "Write reports",
+	},
+	{
+		stream: "J",
+		option: "Help former prison inmates find work",
+	},
+	{
+		stream: "L",
+		option: "Design a freeway",
+	},
+	{
+		stream: "M",
+		option: "Plan educational lessons",
+	},
+	{
+		stream: "N",
+		option: "Balance a checkbook",
+	},
+	{
+		stream: "O",
+		option: "Take an X-ray",
+	},
+	{
+		stream: "P",
+		option: "Write a computer program",
+	},
+	{
+		stream: "Q",
+		option: "Train animals",
+	},
+	{
+		stream: "C",
+		option: "Be in charge of replanting forests",
+	},
+	{
+		stream: "A",
+		option: "Act in a TV show or movie",
+	},
+	{
+		stream: "D",
+		option: "Solve a burglary",
+	},
+	{
+		stream: "F",
+		option: "Check products for quality",
+	},
+	{
+		stream: "E",
+		option: "Build an airport",
+	},
+	{
+		stream: "G",
+		option: "Keep company business records",
+	},
+	{
+		stream: "F",
+		option: "Put together small tools",
+	},
+	{
+		stream: "P",
+		option: "Design a website",
+	},
+	{
+		stream: "M",
+		option: "Tutor students",
+	},
+	{
+		stream: "Q",
+		option: "Work at a zoo",
+	},
+	{
+		stream: "J",
+		option: "Take care of children",
+	},
+	{
+		stream: "O",
+		option: "Plan special diets",
+	},
+	{
+		stream: "A",
+		option: "Choreograph a dance",
+	},
+	{
+		stream: "K",
+		option: "Lobby or show support for a cause",
+	},
+	{
+		stream: "H",
+		option: "Sell clothes",
+	},
+	{
+		stream: "E",
+		option: "Work with your hands",
+	},
+	{
+		stream: "I",
+		option: "Work at an amusement park",
+	},
+	{
+		stream: "N",
+		option: "Sell insurance",
+	},
+	{
+		stream: "I",
+		option: "Learn about ethnic groups",
+	},
+	{
+		stream: "P",
+		option: "Manage an information system",
+	},
+	{
+		stream: "N",
+		option: "Appraise the value of a house",
+	},
+	{
+		stream: "M",
+		option: "File books at the library",
+	},
+	{
+		stream: "E",
+		option: "Make three-dimensional items",
+	},
+	{
+		stream: "D",
+		option: "Analyze handwriting",
+	},
+	{
+		stream: "B",
+		option: "Design indoor sprinkler systems",
+	},
+	{
+		stream: "F",
+		option: "Run a factory sewing machine",
+	},
+	{
+		stream: "G",
+		option: "Develop personnel policies",
+	},
+	{
+		stream: "Q",
+		option: "Train racehorses",
+	},
+	{
+		stream: "D",
+		option: "Guard an office building",
+	},
+	{
+		stream: "H",
+		option: "Run a department store",
+	},
+	{
+		stream: "A",
+		option: "Write for a newspaper",
+	},
+	{
+		stream: "G",
+		option: "Use a calculator",
+	},
+	{
+		stream: "O",
+		option: "Help people at a mental health clinic",
+	},
+	{
+		stream: "L",
+		option: "Remodel old houses",
+	},
+	{
+		stream: "M",
+		option: "Care for young children",
+	},
+	{
+		stream: "D",
+		option: "Locate a missing person",
+	},
+	{
+		stream: "N",
+		option: "Plan estate disbursements/payments",
+	},
+	{
+		stream: "P",
+		option: "Enter data",
+	},
+	{
+		stream: "A",
+		option: "Design a book cover",
+	},
+	{
+		stream: "E",
+		option: "Build toys with written instructions",
+	},
+	{
+		stream: "B",
+		option: "Figure out why someone is sick",
+	},
+	{
+		stream: "R",
+		option: "Fly an airplane",
+	},
+	{
+		stream: "C",
+		option: "Learn how things grow and stay alive",
+	},
+	{
+		stream: "H",
+		option: "Sell cars",
+	},
+	{
+		stream: "I",
+		option: "Work as a restaurant host or hostess",
+	},
+	{
+		stream: "D",
+		option: "Fight fires",
+	},
+	{
+		stream: "G",
+		option: "Keep payroll records for a company",
+	},
+	{
+		stream: "J",
+		option: "Work in a nursing home",
+	},
+	{
+		stream: "G",
+		option: "Hire new staff",
+	},
+	{
+		stream: "O",
+		option: "Run ventilators/breathing machines",
+	},
+	{
+		stream: "R",
+		option: "Drive a taxi",
+	},
+	{
+		stream: "A",
+		option: "Broadcast the news",
+	},
+	{
+		stream: "K",
+		option: "Audit taxes for the government",
+	},
+	{
+		stream: "B",
+		option: "Sort and date dinosaur bones",
+	},
+	{
+		stream: "O",
+		option: "Give shots",
+	},
+	{
+		stream: "C",
+		option: "Design landscaping",
+	},
+	{
+		stream: "P",
+		option: "Give tech support to computer users",
+	},
+	{
+		stream: "D",
+		option: "Work in a courtroom",
+	},
+	{
+		stream: "Q",
+		option: "Care for injured animals",
+	},
+	{
+		stream: "I",
+		option: "Serve meals to customers",
+	},
+	{
+		stream: "L",
+		option: "Build kitchen cabinets",
+	},
+	{
+		stream: "N",
+		option: "Refinance a mortgage",
+	},
+	{
+		stream: "A",
+		option: "Sing in a concert",
+	},
+	{
+		stream: "R",
+		option: "Direct the takeoff/landing of planes",
+	},
+	{
+		stream: "G",
+		option: "Operate a cash register",
+	},
+	{
+		stream: "B",
+		option: "Collect rocks",
+	},
+	{
+		stream: "G",
+		option: "Start a business",
+	},
+	{
+		stream: "L",
+		option: "Draft a blueprint",
+	},
+	{
+		stream: "M",
+		option: "Assess student progress",
+	},
+	{
+		stream: "L",
+		option: "Design an airplane",
+	},
+	{
+		stream: "O",
+		option: "Wrap a sprained ankle",
+	},
+	{
+		stream: "I",
+		option: "Guide an international tour group",
+	},
+	{
+		stream: "P",
+		option: "Solve technical problems",
+	},
+	{
+		stream: "J",
+		option: "Provide spiritual guidance to others",
+	},
+	{
+		stream: "Q",
+		option: "Manage a veterinary clinic",
+	},
+	{
+		stream: "K",
+		option: "Lead others",
+	},
+	{
+		stream: "E",
+		option: "Operate heavy equipment",
+	},
+	{
+		stream: "Q",
+		option: "Manage a fish hatchery",
+	},
+	{
+		stream: "F",
+		option: "Assemble cars",
+	},
+	{
+		stream: "K",
+		option: "Protect our borders",
+	},
+	{
+		stream: "A",
+		option: "Play an instrument",
+	},
+	{
+		stream: "J",
+		option: "Plan activities for adult day care",
+	},
+	{
+		stream: "C",
+		option: "Research soybean use in paint",
+	},
+	{
+		stream: "J",
+		option: "Provide consumer information",
+	},
+	{
+		stream: "D",
+		option: "Guard money in an armored car",
+	},
+	{
+		stream: "B",
+		option: "Study human behavior",
+	},
+	{
+		stream: "E",
+		option: "Fix a television set",
+	},
+	{
+		stream: "M",
+		option: "Run a school",
+	},
+	{
+		stream: "F",
+		option: "Fix a control panel",
+	},
+	{
+		stream: "J",
+		option: "Help friends with personal problems",
+	},
+	{
+		stream: "C",
+		option: "Oversee a logging crew",
+	},
+	{
+		stream: "B",
+		option: "Study weather conditions",
+	},
+	{
+		stream: "R",
+		option: "Pack boxes at a warehouse",
+	},
+	{
+		stream: "A",
+		option: "Teach dancing",
+	},
+	{
+		stream: "O",
+		option: "Sterilize surgical instruments",
+	},
+	{
+		stream: "B",
+		option: "Study soil conditions",
+	},
+	{
+		stream: "N",
+		option: "Play the stock market",
+	},
+	{
+		stream: "C",
+		option: "Protect the environment",
+	},
+	{
+		stream: "M",
+		option: "Grade papers",
+	},
+	{
+		stream: "R",
+		option: "Operate a train",
+	},
+	{
+		stream: "L",
+		option: "Order building supplies",
+	},
+	{
+		stream: "E",
+		option: "Paint motors",
+	},
+	{
+		stream: "P",
+		option: "Develop new computer games",
+	},
+	{
+		stream: "H",
+		option: "Buy merchandise for a store",
+	},
+	{
+		stream: "K",
+		option: "Work to get someone elected",
+	},
+	{
+		stream: "C",
+		option: "Identify plants in a forest",
+	},
+	{
+		stream: "D",
+		option: "Guard inmates in a prison",
+	},
+	{
+		stream: "L",
+		option: "Read blueprints",
+	},
+	{
+		stream: "H",
+		option: "Line up concerts for a band",
+	},
+	{
+		stream: "K",
+		option: "Ask people survey questions",
+	},
+	{
+		stream: "E",
+		option: "Manage a factory",
+	},
+	{
+		stream: "O",
+		option: "Work as a nurse in a hospital",
+	},
+	{
+		stream: "A",
+		option: "Paint a portrait",
+	},
+	{
+		stream: "K",
+		option: "Testify before Congress",
+	},
+	{
+		stream: "B",
+		option: "Work with a microscope",
+	},
+	{
+		stream: "I",
+		option: "Schedule tee times at a golf course",
+	},
+	{
+		stream: "C",
+		option: "Classify plants",
+	},
+	{
+		stream: "O",
+		option: "Transcribe medical records",
+	},
+	{
+		stream: "F",
+		option: "Install rivets",
+	},
+	{
+		stream: "Q",
+		option: "Raise worms",
+	},
+	{
+		stream: "N",
+		option: "Balance accounts",
+	},
+	{
+		stream: "M",
+		option: "Develop learning games",
+	},
+	{
+		stream: "J",
+		option: "Read to sick people",
+	},
+	{
+		stream: "P",
+		option: "Repair computers",
+	},
+	{
+		stream: "F",
+		option: "Compare sizes and shapes of objects",
+	},
+	{
+		stream: "Q",
+		option: "Fish",
+	},
+	{
+		stream: "R",
+		option: "Repair bicycles",
+	},
+	{
+		stream: "K",
+		option: "Deliver mail",
+	},
+	{
+		stream: "M",
+		option: "Teach Special Education",
+	},
+	{
+		stream: "P",
+		option: "Set up a tracking system",
+	},
+	{
+		stream: "G",
+		option: "Manage a store",
+	},
+	{
+		stream: "H",
+		option: "Advertise goods and services",
+	},
+	{
+		stream: "R",
+		option: "Distribute supplies to dentists",
+	},
+	{
+		stream: "I",
+		option: "Compete in a sports event",
+	},
+	{
+		stream: "I",
+		option: "Check guests into a hotel",
+	},
+	{
+		stream: "M",
+		option: "Teach adults to read",
+	},
+	{
+		stream: "L",
+		option: "Follow step-by-step instructions",
+	},
+	{
+		stream: "N",
+		option: "Collect past due bills",
+	},
+	{
+		stream: "R",
+		option: "Inspect cargo containers",
+	},
+	{
+		stream: "F",
+		option: "Work in a cannery",
+	},
+	{
+		stream: "I",
+		option: "Coach a school sports team",
+	},
+	{
+		stream: "P",
+		option: "Update a website",
+	},
+	{
+		stream: "Q",
+		option: "Hunt",
+	},
+	{
+		stream: "K",
+		option: "Enlist in a branch of the military",
+	},
+	{
+		stream: "H",
+		option: "Sell sporting goods",
+	},
+	{
+		stream: "J",
+		option: "Cut and style hair",
+	},
+	{
+		stream: "B",
+		option: "Experiment to find new metals",
+	},
+	{
+		stream: "N",
+		option: "Work in a bank",
+	},
+	{
+		stream: "G",
+		option: "Work with computer programs",
+	},
+	{
+		stream: "N",
+		option: "Loan money",
+	},
+	{
+		stream: "L",
+		option: "Hang wallpaper",
+	},
+	{
+		stream: "D",
+		option: "Make an arrest",
+	},
+	{
+		stream: "O",
+		option: "Deliver babies",
+	},
+	{
+		stream: "H",
+		option: "Persuade people to buy something",
+	},
+	{
+		stream: "H",
+		option: "Stock shelves",
+	},
+	{
+		stream: "I",
+		option: "Serve concession stand drinks",
+	},
+]
+
+const streams = {
+	A: "Arts, A/V Technology and Communications",
+	B: "Science, Technology, Engineering and Mathematics",
+	C: "Plants, Agriculture and Natural Resources",
+	D: "Law, Public Safety, Corrections and Security",
+	E: "Mechanical Manufacturing",
+	F: "Industrial Manufacturing",
+	G: "Business, Management and Administration",
+	H: "Marketing, Sales and Service",
+	I: "Hospitality and Tourism",
+	J: "Human Services",
+	K: "Government and Public Administration",
+	L: "Architecture, Design and Construction",
+	M: "Education and Training",
+	N: "Finance, Banking, Investments and Insurance",
+	O: "Health Sciences, Care and Prevention",
+	P: "Information Technology (IT)",
+	Q: "Animals, Agriculture and Natural Resources",
+	R: "Transportation, Distribution and Logistics",
+}
